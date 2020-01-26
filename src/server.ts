059adb6755b1e8bf07097aeaf4f7348eea7c9216
Maciej Reimann <maciej.reimann@gmail.com>
2020-01-26 14:44:09 +0100

    socket.io installed

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from "express"
+import socket from "socket.io"
 
 const app = express()
+const server = app.listen(5000, () => console.log("server running"))
+const io = socket(server)
 
 app.get("/", (reg, res) => {
   res.send("Hello")
 })
 
-app.listen(5000, () => console.log("server running"))
+io.sockets.on("connection", () => newConnection)
+
 console.log(process.env.NODE_ENV)
+
+function newConnection() {
+  console.log("Connection established")
+}
